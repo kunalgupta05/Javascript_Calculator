279b6b2ce08922e819983cacfefbc4b58548a9fb
Kunal Gupta <kunal@github.com>
2016-08-30 00:23:46 +0530

    Added some more functionality with js

--- js/javator.js
+++ js/javator.js
@@ -1,36 +1,64 @@
-$(document).ready(function(){
-    
-    //Append the text to calculator_result_display except for delete and equals signs. 
-    $('.operators').on('click',function(){
-                
-        if($(this).find('span').hasClass('multiply')){
-            $('.equation').append("*");   
+$(document).ready(function () {
+    var operand = false;
+    var operatorCount = 0;
+
+    //Function to check if the user is not just entering the operator in the display without any operands
+    function checkOperand(operator) {
+        if (operand && operatorCount <= 1) {
+            $('.equation').append(operator);
         }
-        else if($(this).find('span').hasClass('divide')){
-            $('.equation').append("/");
+        else {
+            $('.equation').append("");
         }
-        else{
-            $('.equation').append($(this).find('span').text());         
-        }       
-        
-        evaluateExpression();
-        
-    });
-      
-    $('.delete').on('click',function(){
-        $('.equation').html('');
-        $('.result').html(''); 
-    });  
-      
-    //call the evaluateExpression function when the user clicks on the equals operator.
-    $('.equals').on('click',function(){
-        evaluateExpression();              
-    })
-    
-    //Function which does the magic of evaluating the expression the user types in
-    function evaluateExpression(){
-        var result=eval($('.equation').html());
-        $('.result').text(result); 
     }
-    
+
+    //Append the text to calculator_result_display except for delete and equals signs. 
+    $('.operators').on('click', function () {
+
+        if ($(this).find('span').hasClass('multiply')) {
+            operatorCount++;
+            checkOperand("*");
+        }
+        else if ($(this).find('span').hasClass('divide')) {
+            operatorCount++;
+            checkOperand("/");
+        }
+        else if ($(this).find('span').hasClass('add')) {
+            operatorCount++;
+            checkOperand("+");
+        }
+        else {            
+            operand = true;
+            if ($(this).find('span').hasClass('subtract')) {
+                operatorCount++;
+                checkOperand("-");
+            }
+            else{
+               operatorCount = 0;
+               $('.equation').append($(this).find('span').text());    
+            }            
+        }
+
+        evaluateExpression();
+
+    });
+
+    $('.delete').on('click', function () {
+        operatorCount = 0;
+        operand = false;
+        $('.equation').html('');
+        $('.result').html('');
+    });
+
+    //call the evaluateExpression function when the user clicks on the equals operator.
+    $('.equals').on('click', function () {
+        evaluateExpression();
+    })
+
+    //Function which does the magic of evaluating the expression the user types in
+    function evaluateExpression() {
+        var result = eval($('.equation').html());
+        $('.result').text(result);
+    }
+
 })
